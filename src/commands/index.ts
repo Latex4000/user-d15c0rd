f12eab25addce4cb0bccffba40428a6aeb18a3c7
VINXIS <oykxf2@gmail.com>
2025-01-08 15:24:07 -0700

    ping command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
 import { AttachmentPayload, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
-import upload from "./upload";
-import join from "./join";
 import confirm from "./confirm";
+import join from "./join";
+import ping from "./ping";
+import upload from "./upload";
 
 export interface Command {
     data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
@@ -10,8 +11,9 @@ export interface Command {
 
 const commands: Command[] = [];
 
-commands.push(upload);
-commands.push(join);
 commands.push(confirm);
+commands.push(join);
+commands.push(ping);
+commands.push(upload);
 
 export { commands };
---
+++ src/commands/ping.ts
@@ -0,0 +1,15 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from ".";
+
+const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Ping the bot")
+        .setDMPermission(false),
+    run: async (interaction: ChatInputCommandInteraction) => {
+        await interaction.deferReply();
+        await interaction.followUp({ content: `Yo, this is being sent at ${Date.now() - interaction.createdTimestamp}ms!` });
+    },
+}
+
+export default command;
