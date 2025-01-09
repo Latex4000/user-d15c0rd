b6c696cea99eedc6494f143e77775a8f8f3f8014
VINXIS <oykxf2@gmail.com>
2025-01-08 19:16:01 -0700

    add main site to ring

--- src/data/webringCode.ts
+++ src/data/webringCode.ts
@@ -1,11 +1,22 @@
+
+import * as config from "../../config.json";
+
 export interface Member {
     discord: string;
     alias: string;
     aliasEncoded: string;
     site: string;
     addedRingToSite: boolean;
-} 
+}
+
+const mainSite: Member = {
+    discord: "",
+    alias: config.collective.name,
+    aliasEncoded: config.collective.name_condensed,
+    site: config.collective.site_url,
+    addedRingToSite: true,
+}
 
 export function webringJS (sites: Member[]): string {
-    return `(()=>{let e=${JSON.stringify(sites)};if(!Array.isArray(e))return;e=e.filter((e=>e.addedRingToSite));const t=e.findIndex((e=>e.aliasEncoded===document.getElementById("latex4000Webring").dataset.alias.toLowerCase()));if(-1===t)return;document.getElementById("latex4000Prev").href=e[(t-1+e.length)%e.length].site;const n=n=>{let r=Math.floor(Math.random()*(e.length-1));r>=t&&r++,n.currentTarget.href=e[r].site},r=document.getElementById("latex4000Random");r.addEventListener("click",n),r.addEventListener("auxclick",n),document.getElementById("latex4000Next").href=e[(t+1)%e.length].site})();`
+    return `(()=>{let e=${JSON.stringify([mainSite, ...sites])};if(!Array.isArray(e))return;e=e.filter((e=>e.addedRingToSite));const t=e.findIndex((e=>e.aliasEncoded===document.getElementById("latex4000Webring").dataset.alias.toLowerCase()));if(-1===t)return;document.getElementById("latex4000Prev").href=e[(t-1+e.length)%e.length].site;const n=n=>{let r=Math.floor(Math.random()*(e.length-1));r>=t&&r++,n.currentTarget.href=e[r].site},r=document.getElementById("latex4000Random");r.addEventListener("click",n),r.addEventListener("auxclick",n),document.getElementById("latex4000Next").href=e[(t+1)%e.length].site})();`
 }
