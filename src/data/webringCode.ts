b3467f880a270e34d5fef335fac00b6ac2714926
VINXIS <oykxf2@gmail.com>
2025-01-11 00:53:21 -0700

    fix random not running on load

--- src/data/webringCode.ts
+++ src/data/webringCode.ts
@@ -18,5 +18,5 @@ const mainSite: Member = {
 }
 
 export function webringJS (sites: Member[]): string {
-    return `(()=>{let e=${JSON.stringify([mainSite, ...sites])};if(!Array.isArray(e))return;e=e.filter((e=>e.addedRingToSite));const t=e.findIndex((e=>e.aliasEncoded===document.getElementById("latex4000Webring").dataset.alias.toLowerCase()));if(-1===t)return;document.getElementById("latex4000Prev").href=e[(t-1+e.length)%e.length].site;const n=n=>{let r=Math.floor(Math.random()*(e.length-1));r>=t&&r++,n.currentTarget.href=e[r].site},r=document.getElementById("latex4000Random");r.addEventListener("load",n),document.getElementById("latex4000Next").href=e[(t+1)%e.length].site})();`
+    return `(()=>{let e=${JSON.stringify([mainSite, ...sites])};if(!Array.isArray(e))return;e=e.filter((e=>e.addedRingToSite));const t=e.findIndex((e=>e.aliasEncoded===document.getElementById("latex4000Webring").dataset.alias.toLowerCase()));if(-1===t)return;document.getElementById("latex4000Prev").href=e[(t-1+e.length)%e.length].site;let n=Math.floor(Math.random()*(e.length-1));n>=t&&n++;document.getElementById("latex4000Random").href=e[n].site;document.getElementById("latex4000Next").href=e[(t+1)%e.length].site})();`
 }
