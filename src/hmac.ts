86df70b8146b3beb49c443e6f0ad90b40e54f503
VINXIS <oykxf2@gmail.com>
2025-01-18 22:30:56 -0700

    allow various methods for fetchHMAC

--- src/hmac.ts
+++ src/hmac.ts
@@ -1,12 +1,12 @@
 import * as config from "../config.json";
 import crypto from "crypto";
 
-export function fetchHMACPOST (url: string | URL | globalThis.Request, body: any) {
+export function fetchHMAC (url: string | URL | globalThis.Request, method: string = "POST", body: any) {
     const req = JSON.stringify(body);
     const timestamp = Date.now().toString();
     const hmac = crypto.createHmac("sha256", config.secret_hmac).update(`${timestamp}.${req}`).digest("hex");
     return fetch(url, {
-        method: "POST",
+        method,
         headers: {
             "Content-Type": "application/json",
             "X-Signature": hmac,
